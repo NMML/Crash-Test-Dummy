ce77cc62d4b23f503b5d478c9523c10bf8b211b1
Jeff Laake <jefflaake@gmail.com>
2015-01-15 09:02:43 -0800

    deleted lines

--- test.r
+++ test.r
@@ -9,10 +9,6 @@ R is free software and comes with ABSOLUTELY NO WARRANTY.
 You are welcome to redistribute it under certain conditions.
 Type 'license()' or 'licence()' for distribution details.
 
-R is a collaborative project with many contributors.
-Type 'contributors()' for more information and
-'citation()' on how to cite R or R packages in publications.
-
 Type 'demo()' for some demos, 'help()' for on-line help, or
 'help.start()' for an HTML browser interface to help.
 Type 'q()' to quit R.
